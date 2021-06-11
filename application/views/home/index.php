39d67482142d4f18c5c680995ebb5a2caa5c2450
amzarnatsir <amzarnatsir79@gmail.com>
2021-06-11 16:56:44 +0800

    Update CBT init

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -4,18 +4,18 @@
         <div class="col-md-12">
             <div class="grey-box-icon">
                 <div class="icon-box-top grey-box-icon-pos">
-                    <a href="<?php echo base_url();?>Ujian"><img src="<?php echo base_url();?>assets/images/5.png" width="150px" /></a>
+                    <a href="<?php echo base_url();?>ujian"><img src="<?php echo base_url();?>assets/images/5.png" width="150px" /></a>
                 </div><!--icon box top -->
-                <h4><a href="<?php echo base_url();?>Ujian">Login Peserta Ujian</a></h4>
+                <h4><a href="<?php echo base_url();?>ujian">Login Peserta Ujian</a></h4>
                 
             </div><!--grey box -->
         </div><!--/span3-->
         <div class="col-md-12">
             <div class="grey-box-icon">
                 <div class="icon-box-top grey-box-icon-pos">
-                    <a href="<?php echo base_url();?>Pengawas"><img src="<?php echo base_url();?>assets/images/6.png" width="150px" /></a>
+                    <a href="<?php echo base_url();?>pengawas"><img src="<?php echo base_url();?>assets/images/6.png" width="150px" /></a>
                 </div><!--icon box top -->
-                <h4><a href="<?php echo base_url();?>Pengawas">Login Pengawas Ujian</a></h4>
+                <h4><a href="<?php echo base_url();?>pengawas">Login Pengawas Ujian</a></h4>
             </div><!--grey box -->
         </div><!--/span3-->
     </div>
